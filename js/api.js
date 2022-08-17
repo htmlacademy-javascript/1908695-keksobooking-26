f0fc8548b4ae515a7edcd7fbd6d2cd1e8984e74f
olgadeveloper <madonovaolga@icloud.com>
2022-08-17 15:55:03 +0700

    linter fixed

--- js/api.js
+++ js/api.js
@@ -2,7 +2,6 @@ const getData = (onSuccess, onFail) => {
   fetch('https://26.javascript.pages.academy/keksobooking/data')
     .then((response) => {
       if (response.ok) {
-        console.log(response);
         return response.json();
       } else {
         onFail();
